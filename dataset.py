f80c8669695c1e3230acc22c85828a401c36e420
NasrinKalanat <NAK168@pitt.edu>
2024-09-23 17:08:13 -0400

    bugs fixed

--- dataset.py
+++ dataset.py
@@ -203,6 +203,7 @@ class ImageNetVidDataset(torch.utils.data.Dataset):
             with open(path, 'rb') as f:
                 img = np.load(path, allow_pickle=True)
                 # Convert the image from BGR to RGB (if necessary)
+                img = cv2.resize(img, 256, interpolation=cv2.INTER_CUBIC)
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 img = (img.astype(np.float32) / 127.5) - 1.0
                 return img
