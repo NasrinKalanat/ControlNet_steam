b0963ff5c27b2120cc7ad6c2345e36ea8a903343
NasrinKalanat <NAK168@pitt.edu>
2024-09-23 18:17:27 -0400

    bugs fixed

--- dataset.py
+++ dataset.py
@@ -547,7 +547,7 @@ class ImageNetVidDataset(torch.utils.data.Dataset):
             t[i] = torch.tensor([self.dates[id][i][0]])
             lbl[i] = torch.tensor([self.labels[id][i][0]])
             w[i] = np.array(self.weather[id][i][:])
-            weather_cond = [item.replace('/', ' or ') for item in w[i]]
+            weather_cond = [item.lstrap('/') for item in self.weather_label[id][i][:]]
             weather_cond = ' and '.join(weather_cond)
             print(np.array(weather_cond))
             wlbl[i] = np.array(self.weather_label[id][i][:])
