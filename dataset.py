d177315c14c46a6746d60bce6620c09e5e552aee
NasrinKalanat <NAK168@pitt.edu>
2024-09-23 19:08:13 -0400

    bugs fixed

--- dataset.py
+++ dataset.py
@@ -553,7 +553,6 @@ class ImageNetVidDataset(torch.utils.data.Dataset):
             wlbl.append([weather_cond])
             ids[i] = self.images[id][i][2]
 
-        print(wlbl)
         #     return imgs.squeeze(), t, torch.from_numpy(self.labels[id].astype(np.float32)), w, torch.from_numpy(ids)
         # w = torch.tensor(w)
         # wlbl = torch.tensor(wlbl)
@@ -573,7 +572,7 @@ class ImageNetVidDataset(torch.utils.data.Dataset):
         wlabel_X = wlbl[:-1]
         ids_X = ids[:-1]
         if self.len_seq == 1:
-            return dict(jpg = images_nxt, txt = wlabel_nxt, hint = images_X)
+            return dict(jpg = images_nxt.squeeze(1), txt = wlabel_nxt.squeeze(1), hint = images_X.squeeze(1))
         return dict(jpg = images_nxt, txt = wlabel_nxt, hint = images_X)
         #     return {"img": images_nxt.squeeze(0),
         #             "mixed": {"cond": (images_X.squeeze(0), label_X.reshape(self.batch_size, self.len_seq, -1).squeeze(0), weather_X.reshape(self.batch_size, self.len_seq, -1).squeeze(0),
