477f9dd4dbddf128de5218ef7ee24872298917b7
NasrinKalanat <NAK168@pitt.edu>
2024-09-24 22:40:12 -0400

    bugs fixed

--- dataset_eval.py
+++ dataset_eval.py
@@ -2,6 +2,7 @@ import os
 import glob
 from PIL import Image
 from torchvision import transforms
+import json
 class DatasetEval(object):
     def __init__(self, imgs_gt_path, imgs_gen_path, image_size=256):
         self.transform = transforms.Compose([
@@ -17,22 +18,36 @@ class DatasetEval(object):
         self.ids = []
         for file in glob.glob(f"{imgs_gt_path}/*.png"):
             file = os.path.basename(file)
-            if "control" in file:
-                _, idx = file.split(".png")[0].split("_gs-")
+            _, idx = file.split(".png")[0].split("_gs-")
+            if file.startswith("control"):
                 self.imgs_gt[idx]=file
-            # self.labels[idx]=lbl
-            # self.ids.append(idx)
-            if "samples" in file:
+            if file.startswith("label"):
+                with open(f"{imgs_gen_path}/{file}", "r") as f:
+                    self.labels[idx]=json.load(f)
+                for i in range(len(self.labels[idx])):
+                    self.ids.append((idx,len(self.labels[idx]), i))
+            if file.startswith("samples"):
                 _, idx = file.split(".png")[0].split("_")
                 self.imgs_gen[idx]=file
 
+    def split_img(self, img_gt_path, l, i):
+        img = Image.open(img_gt_path)
+        w, h = img.size
+        w_per_img = w //4
+        return img.crop((i*w_per_img,0,(i+1)*w_per_img,h))
     def __getitem__(self, id):
-        img_gt_path = os.path.join(self.imgs_gt_path, self.imgs_gt[self.ids[id]])
-        img_gen_path = os.path.join(self.imgs_gen_path, self.imgs_gen[self.ids[id]])
-        lbl = self.labels[self.ids[id]]
+        idx, l, i = self.ids[id]
+        img_gt_path = os.path.join(self.imgs_gt_path, self.imgs_gt[idx])
+        img_gen_path = os.path.join(self.imgs_gen_path, self.imgs_gen[idx])
+        lbl = self.labels[idx]
 
-        img_gt = self.transform(Image.open(img_gt_path))
-        img_gen = self.transform(Image.open(img_gen_path))
+        for i in range(4):
+            img_gt = self.split_img(img_gt_path, l, i)
+            img_gt.save(f"test_{idx}_{i}.png")
+        exit()
+        img_gt = self.transform(img_gt)
+        img_gen = self.split_img(img_gen_path, l, i)
+        img_gen = self.transform(img_gen)
         return img_gt, img_gen, lbl
 
     def __len__(self):
